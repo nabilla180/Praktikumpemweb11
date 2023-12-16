df96fcf0af491fedc0acadf63eb9c077fa6dc403
Miatul Nabilla <145973698+nabilla180@users.noreply.github.com>
2023-12-16 08:35:38 -0800

    Add files via upload

---
+++ latihan.js
@@ -0,0 +1,66 @@
+// ini variabel dengan let
+let nama = "Fadhlurrohman Zuhdi"
+console.logo(nama)
+
+// ini variabel dengan const
+const firstname = "Fadhlurrohman"
+const lastname = "zuhdi"
+console.log(firstName + " " + lestName)
+
+// ini tipedata string
+const gender = "LK"
+console.log(typeof gender)
+
+// ini tipedata int
+const age = 21
+console.log(age)
+console.log(typeof age)
+
+// tipedata boolean
+const isMarried = false;
+console.log(isMarried)
+console.log(typeof isMarried)
+
+// Operator Aritmatika
+const x = 9
+const y = 10
+
+// pertambahan
+const z = x + y
+
+// pengurangan
+const m = x - y
+
+//perkalian
+const n = x * y
+
+// pembagian
+const o = x / y
+
+//pembagian
+const c = 10 % 2
+
+console.log(z)
+console.log(m)
+console.log(n)
+console.log(o)
+console.log(c)
+
+// function
+function salam() {
+    alert("Assalamualaikum")
+}
+
+salam();
+
+function isTrue() {
+    let city ="Depok"
+}
+console.log(city)
+
+isTrue;
+
+// tipedata boolean
+
+ 
+ 
